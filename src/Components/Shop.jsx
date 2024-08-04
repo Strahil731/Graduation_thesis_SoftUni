00310a939cf8e61a4d3dcc56caab8e21b4e5682b
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-08-04 11:52:12 +0300

    Update Shop.jsx

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
 import "../Styles/shop.css";
+import { CiHeart } from "react-icons/ci";
+import { TfiReload } from "react-icons/tfi";
+import { CiSearch } from "react-icons/ci";
 
-export default function Shop() {
+export default function Shop({ addToCart }) {
+
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        async function showProduct() {
+            const response = await fetch("https://myshop-a6615-default-rtdb.europe-west1.firebasedatabase.app/.json", {
+                method: "GET"
+            });
+            const data = await response.json();
+
+            setProduct(data);
+        }
+
+        showProduct();
+    }, []);
+
     return (
         <>
             <div className="shop">
@@ -28,6 +48,38 @@ export default function Shop() {
                                     src="https://static.vecteezy.com/system/resources/previews/004/299/818/non_2x/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg"
                                     alt="banner"
                                 />
+                            </div>
+                            <h2>Shop Now</h2>
+                            <div className="product_container">
+                                {
+                                    product.map((currentEl) => {
+                                        return (
+                                            <>
+                                                <div className="box">
+                                                    <div className="img_box">
+                                                        <img src={currentEl.img} alt="image" />
+                                                    </div>
+                                                    <div className="detail">
+                                                        <div className="icons">
+                                                            <div className="icon">
+                                                                <CiHeart />
+                                                            </div>
+                                                            <div className="icon">
+                                                                <TfiReload />
+                                                            </div>
+                                                            <div className="icon">
+                                                                <CiSearch />
+                                                            </div>
+                                                        </div>
+                                                        <h3>{currentEl.Name}</h3>
+                                                        <h4>${currentEl.price}</h4>
+                                                        <button onClick={() => addToCart(currentEl)}>Add To Cart</button>
+                                                    </div>
+                                                </div>
+                                            </>
+                                        );
+                                    })
+                                }
                             </div>
                         </div>
                     </div>
